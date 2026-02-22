c309a8dd28fa3bbfa5655be1bf849e4e4dcf4016
Zoey Hewll <zoeyhewll@gmail.com>
2026-02-23 00:15:16 +0800

    add lineage entry as id source

--- majjik.el
+++ majjik.el
@@ -2809,6 +2809,10 @@ Also sets `jj--current-status' in the initial buffer when the status process com
      (jj-log-header-change-id (jj-log-entry-header cmt)))
     ((and cmt (pred jj-log-header-p))
      (jj-log-header-change-id cmt))
+    ((and cmt (pred jj-log-plain-p))
+     (jj-log-plain-change-id cmt))
+    ((and lin (pred jj-status-lineage-entry-p))
+     (jj-status-lineage-entry-change-id lin))
     (unmatched (jj-read-revset prompt))))
 ;; revset:1 ends here
 
